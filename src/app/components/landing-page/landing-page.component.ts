7d591768f162c355ceacf3b93bb50840ae492aac
“Janardhanarao <janardhanarao.burle@qib.com.qa>
2022-01-17 21:44:58 +0300

    profile development: Animations are added

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  animate,
+  keyframes,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
 declare var Typed: any;
 
 @Component({
   selector: "app-landing-page",
   templateUrl: "./landing-page.component.html",
   styleUrls: ["./landing-page.component.scss"],
+  animations: [
+    trigger("bounce", [
+      transition("* => active", [
+        animate(
+          "3s",
+          keyframes([
+            style({ transform: "scale(0.9, 1.1) translateY(-100px)" }),
+            style({ transform: "scale(1.05, 0.95) translateY(0)" }),
+            style({ transform: "scale(1,1) translateY(-7px)" }),
+            style({ transform: "scale(1,1) translateY(0)" }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, AfterViewInit {
   constructor() {}
 
-  ngOnInit() {
-    // typing text animation script
+  bounceDivState = "initial";
+
+  ngOnInit(): void {
+    this.bounceDivState = "active";
+  }
+
+  ngAfterViewInit(): void {
     var typed = new Typed(".typing", {
       strings: [
         "Flutter Developer",
