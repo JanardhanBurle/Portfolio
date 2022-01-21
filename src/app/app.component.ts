3a5a015bca3e726b72ec3975db400d58f159d763
“Janardhanarao <janardhanarao.burle@qib.com.qa>
2022-01-21 23:45:56 +0300

    loader and about me section is added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,54 @@
-import { Component } from "@angular/core";
+import {
+  animate,
+  keyframes,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
+  animations: [
+    trigger("bounce", [
+      transition("* => active", [
+        animate(
+          "3s",
+          keyframes([
+            style({ transform: "scale(0.9, 1.1) translateY(-100px)" }),
+            style({ transform: "scale(1, 0.95) translateY(0)" }),
+            style({ transform: "scale(1,1) translateY(-7px)" }),
+            style({ transform: "scale(1,1) translateY(0)" }),
+          ])
+        ),
+      ]),
+    ]),
+    trigger("slideIn", [
+      state("void", style({ transform: "translateX(100px)" })),
+      transition(":enter", [animate(500)]),
+    ]),
+    trigger("hideShow", [
+      state("true", style({ opacity: 0 })),
+      transition("0 => 1", animate(300)),
+    ]),
+    trigger("fade", [
+      state("void", style({ opacity: 0 })),
+      transition(":enter", [animate(1000)]),
+    ]),
+  ],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  loading = true;
+  bounceDivState = "initial";
+  showContent = false;
+
+  ngOnInit(): void {
+    this.bounceDivState = "active";
+    setTimeout(() => {
+      this.loading = false;
+    }, 3000);
+  }
+}
