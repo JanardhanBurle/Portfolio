7d7f2748dbd3c8f0fd31db2a0f5b0055e784acb0
janardhan <janardhanarao.burle@qib.com.qa>
2024-02-10 12:38:45 +0300

    switched api from heroku to firebase

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -17,7 +17,7 @@ export class HttpService {
 
   post(request: any) {
     return this.http
-      .post("https://reachout-to-me.herokuapp.com/api/mail", request, {
+      .post("https://reach-out-to-me.web.app/api/mail", request, {
         headers: this.httpHeaders,
         responseType: "json",
       })
